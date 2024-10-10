7ef8a017eb1e0f857b930192256486880c420d35
quangthanhphi <phithanh003@gmail.com>
2024-10-10 11:13:04 +0700

    First Commit

---
+++ Api/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using Api.Dtos;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ChatService _chatService;
+        public ChatController(ChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        [HttpPost("register-user")]
+        public IActionResult RegisterUser(UserDto model)
+        {
+            if (_chatService.AddUserToList(model.Name))
+            {
+                //204 status code
+                return NoContent();
+            }
+
+            return BadRequest("This name is taken, choose another name");
+        }
+    }
+}
+
---
+++ Api/Services/ChatService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Services
+{
+	public class ChatService
+	{
+		// Key, value eg: {"abc", "abc@123"}, {"fff", "fafafa212"}
+		private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
+
+		public bool AddUserToList (string userToAdd)
+		{
+			lock(Users)
+			{
+				foreach(var users in Users)
+				{
+					if(users.Key.ToLower() == userToAdd.ToLower())
+					{
+						return false;
+					}
+				}
+
+				Users.Add(userToAdd, null);
+				return true;
+			}
+		}
+
+		public void AddUserConnectinId(string user, string connectionId)
+		{
+			lock(Users)
+			{
+				if(Users.ContainsKey(user))
+				{
+					Users[user] = connectionId;
+				}
+			}
+		}
+
+		public string GetUserByConnectionId(string connectionId)
+		{
+			lock(Users)
+			{
+				return Users.Where(x => x.Value == connectionId).Select(x => x.Key).FirstOrDefault();
+			}
+		}
+
+        public string GetConnectionIdByUser(string user)
+        {
+            lock (Users)
+            {
+                return Users.Where(x => x.Key == user).Select(x => x.Value).FirstOrDefault();
+            }
+        }
+
+		public void RemoveUserFromList(string user)
+		{
+			lock(Users)
+			{
+				if (Users.ContainsKey(user))
+				{
+					Users.Remove(user);
+				}
+			}
+		}
+
+		public string[] GetOnlineUsers()
+		{
+			lock(Users)
+			{
+				return Users.OrderBy(x => x.Key).Select(x => x.Key).ToArray();
+			}
+		}
+    }
+	
+}
+
